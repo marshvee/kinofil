7ee8bb299df8fa78f76320dc9d3a630a62cb22d4
marianars013 <m.rodriguez21@uniandes.edu.co>
2020-03-02 12:05:26 -0500

    Fix env

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-process.env.secret = "webuniandes2020";
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
